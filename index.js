da168c4520d3b704246138aa31948cd263a2dcd6
Jblack017 <blackhamjoshua@comcast.net>
2021-03-11 21:51:29 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// moves the DODGER to the left
+// moves the DODGER to the right
+
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4"; 
+dodger.style.bottom = "0px";
+
+
+function moveDodgerLeft() {    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 30}px`;
+    }
+  } 
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 30}px`;
+    }
+  }   
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
