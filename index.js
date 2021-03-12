f9a1d68dbc0a1b5896f7055ac6bf32b35159ea52
Jblack017 <blackhamjoshua@comcast.net>
2021-03-11 21:53:53 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// moves the DODGER to the left
-// moves the DODGER to the right
-
 const dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4"; 
 dodger.style.bottom = "0px";
